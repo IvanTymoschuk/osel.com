fddc81fb9d15e1e29951300bf06327a4ed3028aa
Ivan Tymoschuk <ivantymo99@gmail.com>
2018-06-06 23:36:42 +0300

    add colors

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -30,8 +30,8 @@ namespace WpfApp1
             adverts.Add(new Advertisment("Tovar", "50$", "Dich", "Govnotovary", "Rivne", DateTime.Now));
             View.ItemsSource =adverts;
 
-            Autorization aur = new Autorization();
-            aur.Show();
+            //Autorization aur = new Autorization();
+            //aur.Show();
         }
 
     }
